6beb5897ac5eb650e2ba7c23b067e2f6c3c106dc
Mamadou Mb. Dione <47165947+mbayedione10@users.noreply.github.com>
2025-08-27 13:47:55 +0000

    👷 chore : migrate Astro build from static output to server output with Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: node({ 
-    mode: 'standalone' 
-  }),
+  output: "server",
+  // @ts-ignore
+  adapter: netlify(),
   experimental: {
     liveContentCollections: true,
   },
